e58b3525904e86d443c0ad720170ae00993db06d
ItsMeMisha <m.shlyahin@rambler.ru>
2020-12-17 12:06:57 +0300

    IHashSet.hpp v0.11

--- Hashes/IHashSet.hpp
+++ Hashes/IHashSet.hpp
@@ -1,7 +1,7 @@
 template<typename T>
 class IHashSet {
 public:
-    void add(T key);
-    void remove(T key);
-    bool find(T key);
+    void add(T key) = 0;
+    void remove(T key) = 0;
+    bool find(T key) = 0;
 };
