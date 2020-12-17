d33b00adcd52e036bb1933adac937bc3e0658f75
ItsMeMisha <m.shlyahin@rambler.ru>
2020-12-17 16:50:11 +0300

    IHashSet.hpp v0.15

--- Hashes/IHashSet.hpp
+++ Hashes/IHashSet.hpp
@@ -3,5 +3,5 @@ class IHashSet {
 public:
     virtual void add(const Key& key, const Value& val) = 0;
     virtual void remove(const Key& key) = 0;
-    virtual Value find(const Key& key) = 0;
+    virtual Value find(const Key& key, const Value& defVal) = 0;
 };
